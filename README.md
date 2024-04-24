84761cb71ea05b28e37c28835704dcc4a1fae605
Daniel Ting <danielzting@gmail.com>
2024-04-23 22:38:54 -0500

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# backrooms
+# Backrooms
 CS354H Final Project
+
+Daniel Ting
+Benjamin Gordon
+
+I have completed the course evaluation survey.
--- make.py
+++ make.py
@@ -1,10 +1,13 @@
 import os
+import time
 import glob
 from distutils.dir_util import copy_tree
 import shutil
 
 srcfiles = glob.glob('./src/backrooms/*.ts')
 cmd = 'tsc --allowJs -m ES2020 -t ES2020 --outDir dist --sourceMap --alwaysStrict ' + " ".join(srcfiles) + ' ./src/lib/vue/vue.js '
-print('Building TypeScript: ' + cmd)
+print('Building TypeScript...', end='', flush=True)
+start = time.time()
 os.system(cmd)
 copy_tree('./src/backrooms/static', './dist')
+print('finished at ' + time.strftime('%H:%M:%S', time.localtime()) + ' (took ' + str(time.time() - start).split('.')[0] + 's)')
